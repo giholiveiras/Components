1ed7543e5aca98112be0a9f0300df492ef236aea
giholiveiras <132359809+giholiveiras@users.noreply.github.com>
2024-04-30 16:12:01 -0300

    Components

---
+++ src/css/style.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+      cont:{
+        flexDirection: "row",
+        alignItems: 'center',
+        marginVertical: '0,'
+      },
+      line:{
+        width: '30%',
+        height: 1,
+        backgroundColor: '#EDECEE',
+        marginHorizontal: 10,
+      },
+      text: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#59606B'
+      }
+  });
+  
